b7705c477ccac0cae51f0412f1fc485d5872bc10
Ajinkya Jain <jainajinkya92@gmail.com>
2014-10-07 00:22:09 +0530

    Q.m added

---
+++ Q.m
@@ -0,0 +1,26 @@
+function Q_val = Q(t)
+
+global td tm Q_max
+
+if(t<=td/2)
+    a0 = 0;
+    a1 = -2*Q_max/td ;
+    a2 = -4*Q_max/td^2;
+    a3 = 8*Q_max/td^3;
+    
+elseif(t>td/2 && t<=(tm-td/2))
+    a0 = -Q_max;
+    a1 = 0;
+    a2 = 0;
+    a3 = 0;
+    
+else
+    a0 = -(2*(Q_max*td^4*tm - 4*Q_max*td^3*tm^2 + 4*Q_max*td^2*tm^3 + 6*Q_max*td*tm^2 - 8*Q_max*tm^3))/td^3;
+    a1 = (2*(Q_max*td^4 - 8*Q_max*td^3*tm + 12*Q_max*td^2*tm^2 + 12*Q_max*td*tm - 24*Q_max*tm^2))/td^3;
+    a2 = -(4*(- 2*Q_max*td^3 + 6*Q_max*tm*td^2 + 3*Q_max*td - 12*Q_max*tm))/td^3;
+    a3 = -(8*(- Q_max*td^2 + 2*Q_max))/td^3;
+end
+
+Q_val = a0 + a1*t + a2*t^2 + a3*t^3;
+
+end
