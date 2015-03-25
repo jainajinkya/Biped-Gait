879ec62e843edefbae0d8e50821895af0f59d71e
Ajinkya Jain <jainajinkya92@gmail.com>
2015-03-25 19:38:25 +0530

    Continuous Gait-- Frontal Plane done

--- check1.m
+++ check1.m
@@ -54,90 +54,90 @@ for i = 1:length(t)
 end
 
 
-%% Video for Sagittal Plane
-% figure
-% hold on
-% pause(2);
-% axis( [-0.8 10 -0.05 2]);
-% 
-% ax = gca;
-% % ax.NextPlot = 'replaceChildren';
-% 
-% % vidObj = VideoWriter('Biped-gait.avi');
-% % vidObj.FrameRate= 10;
-% % open(vidObj);
-% 
-% for i=1:length(t)
-%     % Swing Leg
-%     L1 = makeLine([x_c(i,1) x_c(i,3)],[x_b(i,1) x_b(i,3)]);
-%     L2 = makeLine([x_b(i,1) x_b(i,3)],[x_a(i,1) x_a(i,3)]);
-%     L3 = makeLine([x_a(i,1) x_a(i,3)],[x_p(i,1) x_p(i,3)]);
-%     h1 = plot(L1(:,1),L1(:,2));
-%     h2 = plot(L2(:,1),L2(:,2));
-%     h3 = plot(L3(:,1),L3(:,2));
-% 
-% 
-%     % Static Leg
-%     L4 = makeLine([x_d(i,1) x_d(i,3)],[x_c(i,1) x_c(i,3)]);
-%     L5 = makeLine([x_e(i,1) x_e(i,3)],[x_d(i,1) x_d(i,3)]);
-%     L6 = makeLine([x_e(i,1) x_e(i,3)],[x_q(i,1) x_q(i,3)]);
-%     h4 = plot(L4(:,1),L4(:,2),['r']);
-%     h5 = plot(L5(:,1),L5(:,2),['r']);
-%     h6 = plot(L6(:,1),L6(:,2),['r']);
-% 
-%     pause(0.3);
-% %     F = getframe;
-% %     writeVideo(vidObj,F);
-%     delete(h1,h2,h3,h4,h5,h6);
-% 
-% end
-% % close(vidObj);
-% close
+% Video for Sagittal Plane
+figure
+hold on
+pause(2);
+axis( [-0.8 6 -0.05 1]);
+
+ax = gca;
+% ax.NextPlot = 'replaceChildren';
+
+% vidObj = VideoWriter('Biped-gait.avi');
+% vidObj.FrameRate= 10;
+% open(vidObj);
+
+for i=1:length(t)
+    % Swing Leg
+    L1 = makeLine([x_c(i,1) x_c(i,3)],[x_b(i,1) x_b(i,3)]);
+    L2 = makeLine([x_b(i,1) x_b(i,3)],[x_a(i,1) x_a(i,3)]);
+    L3 = makeLine([x_a(i,1) x_a(i,3)],[x_p(i,1) x_p(i,3)]);
+    h1 = plot(L1(:,1),L1(:,2));
+    h2 = plot(L2(:,1),L2(:,2));
+    h3 = plot(L3(:,1),L3(:,2));
+
+
+    % Static Leg
+    L4 = makeLine([x_d(i,1) x_d(i,3)],[x_c(i,1) x_c(i,3)]);
+    L5 = makeLine([x_e(i,1) x_e(i,3)],[x_d(i,1) x_d(i,3)]);
+    L6 = makeLine([x_e(i,1) x_e(i,3)],[x_q(i,1) x_q(i,3)]);
+    h4 = plot(L4(:,1),L4(:,2),['r']);
+    h5 = plot(L5(:,1),L5(:,2),['r']);
+    h6 = plot(L6(:,1),L6(:,2),['r']);
+
+    pause(0.3);
+%     F = getframe;
+%     writeVideo(vidObj,F);
+    delete(h1,h2,h3,h4,h5,h6);
+
+end
+% close(vidObj);
+close
 
 
 % 
 % %% Video for frontal plane
-figure
-hold on
-pause(1);
-axis( [-0.8 0.8 -0.05 1]);
-
-ax = gca;
-ax.NextPlot = 'replaceChildren';
+% figure
+% hold on
+% pause(1);
+% axis( [-0.8 0.8 -0.05 1]);
 % 
-% vidObj1 = VideoWriter('Biped-gait-frontal.avi');
-% vidObj1.FrameRate= 10;
-% open(vidObj1);
-% 
-% 
-for i=1:length(t)
-    %Swing Leg
-    L1 = makeLine([x_c1(i,2) x_c1(i,3)],[x_b(i,2) x_b(i,3)]);
-    L2 = makeLine([x_b(i,2) x_b(i,3)],[x_a(i,2) x_a(i,3)]);
-    L3 = makeLine([x_a(i,2) x_a(i,3)],[x_p(i,2) x_p(i,3)]);
-    L7 = makeLine([x_c(i,2) x_c(i,3)],[x_c1(i,2) x_c1(i,3)]);
-    h1 = plot(L1(:,1),L1(:,2),['b' 'o']);
-    h2 = plot(L2(:,1),L2(:,2), ['b' 'o']);
-    h3 = plot(L3(:,1),L3(:,2), ['b' 'o']);
-    h7 = plot(L7(:,1),L7(:,2), ['b' 'o']);
-    
-    %Static Leg
-    L4 = makeLine([x_d(i,2) x_d(i,3)],[x_c2(i,2) x_c2(i,3)]);
-    L5 = makeLine([x_e(i,2) x_e(i,3)],[x_d(i,2) x_d(i,3)]);
-    L6 = makeLine([x_e(i,2) x_e(i,3)],[x_q(i,2) x_q(i,3)]);
-    L8 = makeLine([x_c(i,2) x_c(i,3)],[x_c2(i,2) x_c2(i,3)]);
-    h4 = plot(L4(:,1),L4(:,2),['r' 'o']);
-    h5 = plot(L5(:,1),L5(:,2),['r' 'o']);
-    h6 = plot(L6(:,1),L6(:,2),['r' 'o']);
-    h8 = plot(L8(:,1),L8(:,2),['r' 'o']);
-    
-    pause(0.3);
-%     F = getframe;
-%     writeVideo(vidObj1,F);
-    delete(h1,h2,h3,h4,h5,h6,h7,h8);
-    
-end
-% close(vidObj1);
+% ax = gca;
+% ax.NextPlot = 'replaceChildren';
+% % 
+% % vidObj1 = VideoWriter('Biped-gait-frontal.avi');
+% % vidObj1.FrameRate= 10;
+% % open(vidObj1);
+% % 
+% % 
+% for i=1:length(t)
+%     %Swing Leg
+%     L1 = makeLine([x_c1(i,2) x_c1(i,3)],[x_b(i,2) x_b(i,3)]);
+%     L2 = makeLine([x_b(i,2) x_b(i,3)],[x_a(i,2) x_a(i,3)]);
+%     L3 = makeLine([x_a(i,2) x_a(i,3)],[x_p(i,2) x_p(i,3)]);
+%     L7 = makeLine([x_c(i,2) x_c(i,3)],[x_c1(i,2) x_c1(i,3)]);
+%     h1 = plot(L1(:,1),L1(:,2),['b' 'o']);
+%     h2 = plot(L2(:,1),L2(:,2), ['b' 'o']);
+%     h3 = plot(L3(:,1),L3(:,2), ['b' 'o']);
+%     h7 = plot(L7(:,1),L7(:,2), ['b' 'o']);
+%     
+%     %Static Leg
+%     L4 = makeLine([x_d(i,2) x_d(i,3)],[x_c2(i,2) x_c2(i,3)]);
+%     L5 = makeLine([x_e(i,2) x_e(i,3)],[x_d(i,2) x_d(i,3)]);
+%     L6 = makeLine([x_e(i,2) x_e(i,3)],[x_q(i,2) x_q(i,3)]);
+%     L8 = makeLine([x_c(i,2) x_c(i,3)],[x_c2(i,2) x_c2(i,3)]);
+%     h4 = plot(L4(:,1),L4(:,2),['r' 'o']);
+%     h5 = plot(L5(:,1),L5(:,2),['r' 'o']);
+%     h6 = plot(L6(:,1),L6(:,2),['r' 'o']);
+%     h8 = plot(L8(:,1),L8(:,2),['r' 'o']);
+%     
+%     pause(0.3);
+% %     F = getframe;
+% %     writeVideo(vidObj1,F);
+%     delete(h1,h2,h3,h4,h5,h6,h7,h8);
+%     
+% end
+% % close(vidObj1);
 
 
 % %% Angular Plotting
--- static_leg.m
+++ static_leg.m
@@ -12,6 +12,8 @@ if(step_inc)
   x_ori(1) = x_ori(1) + Ls; 
 end
 
+t_multi = floor(t/tm);
+
 %%
 x_q = x_ori;
 x_q(1) = x_ori(1) + l3 ;
@@ -29,15 +31,16 @@ x_d(3) = x_ori(3) + (l1)*cos(phi_val);
 
 %% Frontal Plane
 
+cycle_no  = floor(t/tm);
 
-if(t<=tm)
+if(rem(t_multi,2)== 0)
     x_c2(2) = x_ori(2) - Kc*(l1+l2)*sin(Q_val) + l5/2;
     x_c(2) = x_ori(2) - Kc*(l1+l2)*sin(Q_val);
     x_d(2) = x_ori(2) - Kd*l1*sin(Q_val) + l5/2;
     x_e(2) = x_ori(2) + l5/2;
     x_q(2) = x_e(2);
     
-elseif(t>tm)
+elseif(rem(t_multi,2)== 1)
     x_c2(2) = x_ori(2) - Kc*(l1+l2)*sin(Q_val) - l5/2 ;
     x_c(2) = x_ori(2) - Kc*(l1+l2)*sin(Q_val) ;
     x_d(2) = x_ori(2) - Kd*l1*sin(Q_val) - l5/2 ;
