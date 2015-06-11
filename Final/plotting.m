7d6512fa686b2c7ee4c14ac686a8f27e657052c5
Ajinkya Jain <jainajinkya92@gmail.com>
2015-06-12 02:03:59 +0530

    Non-linear system result comparison

--- Final/plotting.m
+++ Final/plotting.m
@@ -1,8 +1,9 @@
 figure
-plot(X_des(:,1),X_des(:,3));
+plot(X_des(:,1),X_des(:,10));
 xlabel('time (s)','FontSize',18,'FontWeight','bold','Color','k')
 ylabel('Value of Angular Speed (rad/s)','FontSize',18,'FontWeight','bold','Color','k')
 hold on
-scatter(X_obt(:,1),X_obt(:,3), 'r');
+scatter(X_obt_1(:,1),X_obt_1(:,10), 'r');
+plot(X_obt_1(:,1),X_obt_1(:,10), 'r');
 legend('Reference Value','System Response')
 set(gca,'FontSize',15);
